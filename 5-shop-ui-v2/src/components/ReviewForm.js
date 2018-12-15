253eb43ec2fbf0000711fb0245b7e52a13678897
Nag-Training <nag-training@NAGABHUSHANAMs-MacBook-Pro.local>
2018-12-15 15:53:45 +0530

    review form

--- 5-shop-ui-v2/src/components/ReviewForm.js
+++ 5-shop-ui-v2/src/components/ReviewForm.js
@@ -1,30 +1,48 @@
 import React, { Component } from 'react';
 
 class ReviewForm extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { isOpen: false }
+    }
+    toggleForm() {
+        let { isOpen } = this.state;
+        this.setState({ isOpen: !isOpen })
+    }
+    renderForm() {
+        let { isOpen } = this.state;
+        if (!isOpen)
+            return (<button className="btn btn-info" onClick={e => this.toggleForm()}><i className="fa fa-plus"></i></button>)
+        else return (
+            <div className="card">
+                <div className="card-header">Review Form</div>
+                <div className="card-body">
+                    <form>
+                        <div className="form-group">
+                            <label>stars</label>
+                            <select className="form-control"></select>
+                        </div>
+                        <div className="form-group">
+                            <label>author</label>
+                            <input className="form-control" type="email" />
+                        </div>
+                        <div className="form-group">
+                            <label>body</label>
+                            <textarea className="form-control"></textarea>
+                        </div>
+                        <button className="btn btn-dark">submit</button>
+                        &nbsp;
+                        <button type="button" onClick={e => this.toggleForm()} className="btn btn-danger">cancel</button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
     render() {
         return (
             <div className="row">
                 <div className="col-8 col-sm-8 col-md-8">
-                    <div className="card">
-                        <div className="card-header">Review Form</div>
-                        <div className="card-body">
-                            <form>
-                                <div className="form-group">
-                                    <label>stars</label>
-                                    <select className="form-control"></select>
-                                </div>
-                                <div className="form-group">
-                                    <label>author</label>
-                                    <input className="form-control" type="email" />
-                                </div>
-                                <div className="form-group">
-                                    <label>body</label>
-                                    <textarea className="form-control"></textarea>
-                                </div>
-                                <button className="btn btn-dark">submit</button>
-                            </form>
-                        </div>
-                    </div>
+                    {this.renderForm()}
                 </div>
             </div>
         );
